d2ac409846e20d67f9bf253d47f1c2b9085ee2dd
thomass238 <thomass238@nycstudents.net>
2026-02-10 10:08:15 -0500

    day 11

--- lalala.py
+++ lalala.py
@@ -5,18 +5,32 @@ t = Turtle()
 t.speed(1000)
 
 
-""" for i in range(60):
-   t.forward(100)
-   t.left(90)
-   t.right(5) """
+# """ for i in range(60):
+#    t.forward(100)
+#    t.left(90)
+#    t.right(5) """
 
-length = 100
-rotate = 90
-def square(x,y):
-    for i in range (4):
-      t.forward(x)
-      t.left(y)
-square(100,90)
+# sidelength = 100
+# rotate = 90
+# def square(x,y):
+#     for i in range (4):
+#       t.forward(x)
+#       t.left(y)
+# square(100,90)
+
+# def doublesquare(iRange):
+#    length = 25
+#    for i in range(iRange):
+#       square
  
+
+
+
+
+
+for square in range(60):
+  t.forward(200)
+  t.right(95)
+
 
 turtle.done()
