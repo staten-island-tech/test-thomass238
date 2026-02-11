9d71b5be9b726dd72e672f74d0492e18fd83bd91
thomass238 <thomass238@nycstudents.net>
2026-02-11 10:16:03 -0500

    Update lalala.py

--- lalala.py
+++ lalala.py
@@ -2,35 +2,32 @@ import turtle
 from turtle import *
 t = Turtle()
 
-t.speed(1000)
+t.speed(100000000000000)
 
 
-# """ for i in range(60):
+# for i in range(60):
 #    t.forward(100)
 #    t.left(90)
-#    t.right(5) """
+#    t.right(5) 
 
 # sidelength = 100
-# rotate = 90
-# def square(x,y):
-#     for i in range (4):
-#       t.forward(x)
-#       t.left(y)
-# square(100,90)
+# # rotate = 90
 
-# def doublesquare(iRange):
-#    length = 25
-#    for i in range(iRange):
-#       square
- 
+def square(x,y):
+    for i in range (4):
+      t.forward(x)
+      t.left(y)
 
-
-
+def doublesquare(iRange):
+    length = 5
+    for i in range(iRange):
+        square(length, 90)
+        length += 25
+doublesquare(5)
 
 
-for square in range(60):
-  t.forward(200)
-  t.right(95)
 
+
+   
 
 turtle.done()
